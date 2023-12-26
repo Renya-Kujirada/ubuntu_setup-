f1cf6814cb5d1c93bd7dc946b992c0498a2c1376
renya <rsmilek5492@gmail.com>
2023-12-27 00:11:13 +0900

    Add installation script for required software

---
+++ install_myLib.bat
@@ -0,0 +1,20 @@
+choco install googlechrome -y
+choco install chrome-remote-desktop-chrome -y
+choco install googlejapaneseinput -y
+
+choco install anaconda3 -y
+choco install vscode -y
+choco install git -y
+
+choco install slack -y
+choco install discord -y
+choco install line -y
+choco install zoom -y
+
+choco install thunderbird -y
+choco install 7zip -y
+choco install adobereader -y
+choco install geforce-game-ready-driver -y
+choco install geforce-experience -y
+
+
